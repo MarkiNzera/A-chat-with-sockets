4e4f56c26804464b96864bfa1745b0e1d6929ba1
MarkiNzera <vstar516@gmail.com>
2024-05-15 22:16:37 -0300

    front end in react

---
+++ back-end/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+io.on('connection', (socket) => {
+    socket.on("message", (data) => {
+        io.emit("showmsg", data);
+    });
+    
+    socket.on("disconnect", () => {
+        console.log("socket disconnected: " + socket.id);
+    });
+
+});
+
+http.listen(8080, () => {
+    console.log('listening on port 8080');
+});
